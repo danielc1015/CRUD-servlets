186fde4a1febb5b5c2a3ddad95120baa7f4210c9
Daniel <danielc1015@gmail.com>
2019-05-02 15:47:38 -0400

    Class category creada

---
+++ src/java/models/Categoria.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author danielcandiapereira
+ */
+public class Categoria {
+    private int id;
+    private String nombre;
+    
+    private static ArrayList<Categoria> listaCategorias = new ArrayList();
+
+    public Categoria(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Categoria(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public Categoria() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    // ** METHODS OF THE INTERFACE **
+    // START HERE
+
+    public ArrayList<Object> listar() {
+        
+        return (ArrayList)listaCategorias;
+        
+    }
+    
+    public void agregar(Object objeto) {
+        int idMayor = 1;
+        
+        if (listaCategorias != null && listaCategorias.size() > 0) {
+            for (Categoria categoria : listaCategorias) {
+                if (categoria.id > idMayor) {
+                    idMayor = categoria.id;
+                }
+            }
+            idMayor++;
+        }
+        
+        Categoria categoria = (Categoria)objeto;
+        
+        listaCategorias.add(new Categoria(idMayor, categoria.nombre));   
+        
+    }
+    
+    
+}
